13b636ddbb63484b88ca31724837dedb66455178
QTT <18962524854@163.com>
2022-11-13 22:03:43 +0800

    整合 openfeign

--- order/src/main/java/com/example/order/controller/OrderController.java
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.example.order.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,12 @@ public class OrderController {
     @RequestMapping("/{id}")
     public Object getOrder(@PathVariable int id) {
         System.out.println("~~~~~~~~~~~~~~~~~" + id);
+        return null;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.DELETE)
+    public Object add(@PathVariable int id) {
+        System.out.println("------------------" + id);
         return null;
     }
 }
--- user/src/main/java/com/example/order/controller/UserController.java
+++ user/src/main/java/com/example/order/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.order.controller;
 
+import com.example.order.openfeign.OrderFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,13 @@ public class UserController {
 
     @Autowired
     private RestTemplate template;
+    @Autowired
+    private OrderFeignService orderFeignService;
 
     @RequestMapping("/order")
     public Object queryOrder() {
-        template.getForObject("http://order-service/order/1", Integer.class);
+//        template.getForObject("http://order-service/order/1", Integer.class);
+        orderFeignService.getOrder(1);
         return null;
     }
 }
